e8b7a298a491f124aa5badee10f08784e02b2c1f
lhckb <luis.carv.cruz@gmail.com>
2022-07-05 07:27:52 -0300

    first commit on java course

---
+++ src/Fundamentals/FirstProgram.java
@@ -0,0 +1,11 @@
+package Fundamentals;
+
+public class FirstProgram {
+/**
+ @author lhckb
+ @since JDK17
+ */
+    public static void main (String[] args) {
+        System.out.println("hello world");
+    }
+}
